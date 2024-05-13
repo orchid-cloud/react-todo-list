ab7ffbf1f56aec8a9b2feb490b574bd5b80c3aab
NZ <n.prokopyk@gmail.com>
2024-05-13 11:53:34 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo list</h1>
+      <div>
+        <input type="text" placeholder="Task..." />
+        <button>Add task</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
